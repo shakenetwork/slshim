25c3e2b3ecabc07e4f5deb193237ef99e729ae11
vyvojar <vyvojar@mail.ru>
2016-08-28 11:52:43 +0200

    typo

--- fixold.bat
+++ fixold.bat
@@ -1,4 +1,3 @@
 reg add HKLM\SYSTEM\CurrentControlSet\services\sppuinotify /f /v Type /t REG_DWORD /d 32
 reg add HKLM\SYSTEM\CurrentControlSet\services\sppsvc /f /v Type /t REG_DWORD /d 16
 reg add HKLM\SYSTEM\CurrentControlSet\services\osppsvc /f /v Type /t REG_DWORD /d 16
-e
